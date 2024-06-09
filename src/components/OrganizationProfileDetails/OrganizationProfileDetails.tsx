22908452be60fec11aed0b7ac87768ed587ce8c7
Pusiclea <lp53593@fer.hr>
2024-06-09 19:25:24 +0200

    organization profile responsiveness done

--- src/components/OrganizationProfileDetails/OrganizationProfileDetails.tsx
+++ src/components/OrganizationProfileDetails/OrganizationProfileDetails.tsx
@@ -1,9 +1,5 @@
 import React, { useEffect, useState } from "react";
-
-import api from "../../api/createAxiosClient";
-
 import "./organizationProfileDetails.css";
-import { string } from "yup";
 
 interface Address {
   townName: string;
@@ -21,7 +17,7 @@ interface OrganizationDetails {
   profileDescription: string;
   address: Address;
   url: string;
-  image: string;
+  image?: string;
   workArea: string[];
 }
 
@@ -29,14 +25,15 @@ interface OrganizationProfileDetailsProps {
   organizationDetails: OrganizationDetails | null;
 }
 
+// eslint-disable-next-line sonarjs/cognitive-complexity
 const OrganizationProfileDetails: React.FC<OrganizationProfileDetailsProps> = ({ organizationDetails }) => {
   const [isSameUser, setIsSameUser] = useState(false);
+  const [expandedCardIndex, setExpandedCardIndex] = useState<number | null>(null);
 
   useEffect(() => {
     const storedUser = localStorage.getItem("user");
     const storedUserId = storedUser ? JSON.parse(storedUser).id : null;
     setIsSameUser(organizationDetails?.id === storedUserId);
-    console.log(isSameUser);
   }, [organizationDetails?.id]);
 
   const textStyle: React.CSSProperties = {
@@ -45,157 +42,156 @@ const OrganizationProfileDetails: React.FC<OrganizationProfileDetailsProps> = ({
     overflowWrap: "break-word",
   };
 
+  const toggleCardExpansion = (index: number) => {
+    setExpandedCardIndex(expandedCardIndex === index ? null : index);
+  };
+
   if (!organizationDetails) {
     return <div>Loading...</div>;
   }
 
   return (
-    <div className="px-30 flex justify-evenly py-12">
-      <div className="relative flex h-[20vw] w-[20vw] flex-col items-start justify-start overflow-hidden rounded-2xl bg-[#F8F26C] p-4 shadow-2xl">
-        <div className="flex flex-col space-y-2">
-          <div className="">
-            <h2 className="title-description-org font-bold text-[#0F182C] ">Kontakt Info</h2>
-            <div className="mt-1 h-1 w-32 bg-[#C1B908]"></div>
-          </div>
-          <div className="space-y-2 ">
-            {organizationDetails.email && (
-              <div className="flex items-center">
-                <img src="../../../assets/svgImages/mail.svg" alt="Email" className="org-details-icon " />
-                <span style={textStyle} className="org-details-text ml-2 break-words text-[#0F182C]">
-                  {organizationDetails.email}
-                </span>
-              </div>
-            )}
-            {organizationDetails.mobilePhone && (
-              <div className="flex items-center">
-                <img src="../../../assets/svgImages/phone.svg" alt="Phone" className="org-details-icon " />
-                <span style={textStyle} className="org-details-text ml-2 break-words  text-[#0F182C]">
-                  {organizationDetails.mobilePhone}
-                </span>
-              </div>
-            )}
-            {organizationDetails.address && (
-              <div className="flex items-center">
-                <img src="../../../assets/svgImages/location.svg" alt="Phone" className="org-details-icon " />
-                <span style={textStyle} className="org-details-text ml-2 break-words  text-[#0F182C]">
-                  {organizationDetails.address.streetName}, {organizationDetails.address.postcode}{" "}
-                  {organizationDetails.address.townName}
-                </span>
-              </div>
-            )}
-            {organizationDetails.type && (
-              <div className="flex items-center">
-                <img src="../../../assets/svgImages/orgHeart.svg" alt="Phone" className="org-details-icon " />
-                <span style={textStyle} className="org-details-text ml-2 break-words  text-[#0F182C]">
-                  {organizationDetails.type}
-                </span>
-              </div>
-            )}
-          </div>
-        </div>
-        <img
-          src="../../../assets/svgImages/logo.svg"
-          alt="Logo"
-          className="absolute left-24 top-32 h-2/3 w-2/3 rotate-[30deg] opacity-30"
-        />
-      </div>
-
-      <div
-        className="relative flex h-[20vw] w-[20vw] flex-col items-start justify-start rounded-2xl p-4 text-white shadow-2xl"
-        style={{ backgroundColor: "#0F182C" }}
-      >
-        <h2 className="title-description-org font-bold">O ORGANIZACIJI...</h2>
-        {organizationDetails.profileDescription && (
-          <p style={textStyle} className="break-text org-details-text mt-4 overflow-auto">
-            {organizationDetails.profileDescription}
-          </p>
-        )}
-        {isSameUser && (
+    <div className="user-details-container flex flex-col items-center justify-center">
+      <div className="profile-header mb-8 flex flex-col items-center justify-center">
+        {organizationDetails.image && (
           <img
-            src="../../../assets/svgImages/editProfile.svg"
-            alt="Edit"
-            className="absolute right-2 top-2 scale-75 transform sm:scale-90 md:scale-100"
-            style={{ zIndex: 10 }}
+            className="user-image rounded-full border-4 border-white"
+            src={`data:image/jpeg;base64,${organizationDetails.image}`}
+            alt="Profile"
           />
         )}
+        <h1 className="user-name mt-4 text-lg font-bold text-[#09115B]">{organizationDetails.name}</h1>
       </div>
-
-      <div
-        className="relative flex h-[20vw] w-[20vw] flex-col items-start justify-start rounded-2xl p-4 text-[#0F182C] shadow-2xl"
-        style={{ backgroundColor: "#99D7E8" }}
-      >
-        <h2 className="title-description-org font-bold">PODRUČJE RADA</h2>
-        <ul className="org-details-text mt-3 list-disc space-y-2 overflow-auto pl-4">
-          {organizationDetails?.workArea?.length > 0 ? (
-            organizationDetails.workArea.map((area, index) => <li key={index}>{area}</li>)
-          ) : (
-            <li style={textStyle} className="org-details-text break-words ">
-              Još uvijek nije odabrao područja rada.
-            </li>
-          )}
-        </ul>
-        {isSameUser ? (
-          <img
-            src="../../../assets/svgImages/editProfile.svg"
-            alt="Edit"
-            className="absolute right-2 top-2 scale-75 transform sm:scale-90 md:scale-100"
-            style={{ zIndex: 10 }}
-          />
-        ) : (
-          <img
-            src="../../../assets/svgImages/logo.svg"
-            alt="Logo"
-            className="absolute right-2 top-2 h-8 w-8 transform"
-            style={{ zIndex: 10 }}
-          />
-        )}
-      </div>
-      <div
-        className="relative flex h-[20vw] w-[20vw] flex-col items-start justify-start overflow-hidden rounded-2xl p-4 text-[#0F182C] shadow-2xl"
-        style={{ backgroundColor: "#E7F4F8" }}
-      >
-        {organizationDetails?.oib && (
-          <div style={{ marginBottom: "20px" }}>
-            <h2 className="title-description-org font-bold">OIB organizacije</h2>
-            <div className="mt-3 list-disc space-y-2 text-sm">
-              <div className="flex items-center">
-                <img src="../../../assets/svgImages/tick.svg" alt="Email" className="org-details-icon " />
-                <span style={textStyle} className="org-details-text ml-2 break-words  text-[#0F182C]">
-                  {organizationDetails.oib}
-                </span>
-              </div>
+      <div className="details-cards flex flex-wrap justify-evenly px-4 py-12">
+        <div
+          className={`card bg-[#F8F26C] ${expandedCardIndex === 0 ? "expanded" : ""}`}
+          onClick={() => toggleCardExpansion(0)}
+        >
+          <div className="flex flex-col space-y-2">
+            <div>
+              <h2 className="title-description font-bold text-[#0F182C]">Kontakt Info</h2>
+              <div className="mt-1 h-1 w-32 bg-[#C1B908]"></div>
             </div>
-          </div>
-        )}
-        {organizationDetails?.url && (
-          <div>
-            <h2 className="title-description-org top-32 font-bold">WEB stranica</h2>
-            <div className="mt-3 list-disc space-y-2 text-sm">
-              {organizationDetails?.url && (
+            <div className="space-y-2">
+              {organizationDetails.email && (
                 <div className="flex items-center">
-                  <img src="../../../assets/svgImages/tick.svg" alt="Email" className="org-details-icon " />
-                  <span style={textStyle} className="org-details-text ml-2 break-words  text-[#0F182C]">
-                    {organizationDetails.url}
+                  <img src="../../../assets/svgImages/mail.svg" alt="Email" className="user-details-icon" />
+                  <span style={textStyle} className="user-details-text ml-2 break-words text-[#0F182C]">
+                    {organizationDetails.email}
+                  </span>
+                </div>
+              )}
+              {organizationDetails.mobilePhone && (
+                <div className="flex items-center">
+                  <img src="../../../assets/svgImages/phone.svg" alt="Phone" className="user-details-icon" />
+                  <span style={textStyle} className="user-details-text ml-2 break-words text-[#0F182C]">
+                    {organizationDetails.mobilePhone}
+                  </span>
+                </div>
+              )}
+              {organizationDetails.address && (
+                <div className="flex items-center">
+                  <img src="../../../assets/svgImages/location.svg" alt="Address" className="user-details-icon" />
+                  <span style={textStyle} className="user-details-text ml-2 break-words text-[#0F182C]">
+                    {organizationDetails.address.streetName}, {organizationDetails.address.postcode}{" "}
+                    {organizationDetails.address.townName}
                   </span>
                 </div>
               )}
             </div>
           </div>
-        )}
-        <img
-          src="../../../assets/svgImages/logo.svg"
-          alt="Logo"
-          className="absolute inset-0 left-16 h-full w-full scale-150 transform object-cover opacity-25"
-          style={{ zIndex: 10 }}
-        />
-        {isSameUser && (
           <img
-            src="../../../assets/svgImages/editProfile.svg"
-            alt="Edit"
-            className="absolute right-2 top-2 scale-75 transform sm:scale-90 md:scale-100"
-            style={{ zIndex: 10 }}
+            src="../../../assets/svgImages/logo.svg"
+            alt="Logo"
+            className="absolute left-24 top-32 h-2/3 w-2/3 rotate-[30deg] opacity-30"
           />
-        )}
+        </div>
+
+        <div
+          className={`card bg-[#0F182C] text-white ${expandedCardIndex === 1 ? "expanded" : ""}`}
+          onClick={() => toggleCardExpansion(1)}
+        >
+          <h2 className="title-description font-bold">O ORGANIZACIJI...</h2>
+          {organizationDetails.profileDescription && (
+            <p style={textStyle} className="break-text user-details-text mt-4 overflow-auto">
+              {organizationDetails.profileDescription}
+            </p>
+          )}
+          {isSameUser && (
+            <img
+              src="../../../assets/svgImages/editProfile.svg"
+              alt="Edit"
+              className="edit-icon absolute right-2 top-2"
+            />
+          )}
+        </div>
+
+        <div
+          className={`card bg-[#99D7E8] ${expandedCardIndex === 2 ? "expanded" : ""}`}
+          onClick={() => toggleCardExpansion(2)}
+        >
+          <h2 className="title-description font-bold">PODRUČJE RADA</h2>
+          <ul className="user-details-text mt-3 list-disc space-y-2 overflow-auto pl-4">
+            {organizationDetails.workArea && organizationDetails.workArea.length > 0 ? (
+              organizationDetails.workArea.map((area, index) => <li key={index}>{area}</li>)
+            ) : (
+              <li style={textStyle} className="user-details-text break-words">
+                Još uvijek nisu odabrali područja rada.
+              </li>
+            )}
+          </ul>
+          {isSameUser ? (
+            <img
+              src="../../../assets/svgImages/editProfile.svg"
+              alt="Edit"
+              className="edit-icon absolute right-2 top-2"
+            />
+          ) : (
+            <img
+              src="../../../assets/svgImages/logo.svg"
+              alt="Logo"
+              className="absolute right-2 top-2 h-8 w-8 transform"
+              style={{ zIndex: 10 }}
+            />
+          )}
+        </div>
+
+        <div
+          className={`card bg-[#E7F4F8] ${expandedCardIndex === 3 ? "expanded" : ""}`}
+          onClick={() => toggleCardExpansion(3)}
+        >
+          <h2 className="title-description font-bold">OIB & URL</h2>
+          <div className="mt-3 list-disc space-y-2 text-sm">
+            {organizationDetails.oib && (
+              <div className="flex items-center">
+                <img src="../../../assets/svgImages/tick.svg" alt="OIB" className="user-details-icon" />
+                <span style={textStyle} className="user-details-text ml-2 break-words text-[#0F182C]">
+                  {organizationDetails.oib}
+                </span>
+              </div>
+            )}
+            {organizationDetails.url && (
+              <div className="flex items-center">
+                <img src="../../../assets/svgImages/tick.svg" alt="URL" className="user-details-icon" />
+                <span style={textStyle} className="user-details-text ml-2 break-words text-[#0F182C]">
+                  {organizationDetails.url}
+                </span>
+              </div>
+            )}
+          </div>
+          <img
+            src="../../../assets/svgImages/logo.svg"
+            alt="Logo"
+            className="absolute inset-0 left-16 h-full w-full scale-150 transform object-cover opacity-25"
+          />
+          {isSameUser && (
+            <img
+              src="../../../assets/svgImages/editProfile.svg"
+              alt="Edit"
+              className="edit-icon absolute right-2 top-2"
+            />
+          )}
+        </div>
       </div>
     </div>
   );
--- src/pages/OrganizationProfile/OrganizationProfile.tsx
+++ src/pages/OrganizationProfile/OrganizationProfile.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import { useParams } from "react-router-dom";
 
@@ -7,7 +7,6 @@ import api from "../../api/createAxiosClient";
 import OrganizationProfileDetails from "../../components/OrganizationProfileDetails/OrganizationProfileDetails";
 import Reviews from "../../components/Reviews/Reviews";
 import UserGallery from "../../components/UserGallery/UserGallery";
-import UserProfileDetails from "../../components/UserProfileDetails/UserProfileDetails";
 
 interface UserRecension {
   recension: string;
@@ -30,7 +29,7 @@ interface OrganizationDetails {
   address: Address;
   userType: string;
   url: string;
-  image: string;
+  image?: string;
   workArea: string[];
   userRecensions: UserRecension[];
 }
@@ -41,24 +40,23 @@ const OrganizationProfile = () => {
   const [organizationDetails, setOrganizationDetails] = useState<OrganizationDetails | null>(null);
   const [userRecensions, setUserRecensions] = useState<UserRecension[]>([
     {
-      recension:
-        "Ovo je tekst recenzije 1.Ovo je tekst recenzije 1.Ovo je tekst recenzije 1.Ovo je tekst recenzije 1.Ovo je tekst recenzije 1.Ovo je tekst recenzije 1.Ovo je tekst recenzije 1.Ovo je tekst recenzije 1.Ovo je tekst recenzije 1.Ovo je tekst recenzije 1.Ovo je tekst recenzije 1.Ovo je tekst recenzije 1.",
+      recension: "Ovo je tekst recenzije 1...",
     },
     {
-      recension:
-        "Ovo je tekst recenzije 2.Ovo je tekst recenzije 2.Ovo je tekst recenzije 2.Ovo je tekst recenzije 2.Ovo je tekst recenzije 2.Ovo je tekst recenzije 2.Ovo je tekst recenzije 2.Ovo je tekst recenzije 2.Ovo je tekst recenzije 2.Ovo je tekst recenzije 2.Ovo je tekst recenzije 2.Ovo je tekst recenzije 2.Ovo je tekst recenzije 2.",
+      recension: "Ovo je tekst recenzije 2...",
     },
-    { recension: "Ovo je tekst recenzije 3." },
-    { recension: "Ovo je tekst recenzije 4." },
-    { recension: "Ovo je tekst recenzije 5." },
-    { recension: "Ovo je tekst recenzije 6." },
-    { recension: "Ovo je tekst recenzije 7." },
-    { recension: "Ovo je tekst recenzije 9." },
+    { recension: "Ovo je tekst recenzije 3..." },
+    {
+      recension: "Ovo je tekst recenzije 4...",
+    },
+    { recension: "Ovo je tekst recenzije 5..." },
+    { recension: "Ovo je tekst recenzije 6..." },
+    { recension: "Ovo je tekst recenzije 7..." },
+    { recension: "Ovo je tekst recenzije 8..." },
   ]);
   const { orgId } = useParams<{ orgId: string }>();
 
   useEffect(() => {
-    console.log(orgId);
     if (orgId) {
       fetchOrganizationDetails();
     }
@@ -67,16 +65,14 @@ const OrganizationProfile = () => {
   const fetchOrganizationDetails = async () => {
     try {
       const response = await api.get(`/profile/organization/${orgId}`, {
-        headers: {
-          "Content-Type": "application/json",
-        },
+        headers: { "Content-Type": "application/json" },
       });
-      console.log(response.data);
-      setOrganizationDetails(response.data);
-      if (response.data.image !== null) {
-        setOrgImage(response.data.image);
+      const data = response.data;
+      setOrganizationDetails(data);
+      if (data.image !== null) {
+        setOrgImage(data.image);
       }
-      setName(response.data.name);
+      setName(data.name);
     } catch (error) {
       console.error("Error fetching organization details:", error);
     }
@@ -84,24 +80,11 @@ const OrganizationProfile = () => {
 
   return (
     <div className="org-profile relative w-full overflow-hidden">
-      <img src="../../../assets/svgImages/profile_wave.svg" className="wave-background-org " alt="Wave Background" />
-      <div className="content-container-org relative w-full">
-        <div className="profile-image-container-org flex flex-col items-center justify-center ">
-          {orgImage && (
-            <img
-              className="user-image-org rounded-full border-4 border-white "
-              src={`data:image/jpeg;base64,${orgImage}`}
-              alt="Profile"
-            />
-          )}
-          <h1
-            style={{ marginTop: orgImage ? "" : "18vw" }}
-            className="user-name-org mt-4 text-lg font-bold text-[#09115B]"
-          >
-            {name}
-          </h1>
-        </div>
-        <OrganizationProfileDetails organizationDetails={organizationDetails} />
+      <img src="../../../assets/svgImages/profile_wave.svg" className="wave-background" alt="Wave Background" />
+      <div className={`content-container relative w-full ${orgImage ? "with-image" : "without-image"}`}>
+        {organizationDetails && (
+          <OrganizationProfileDetails organizationDetails={{ ...organizationDetails, image: orgImage }} />
+        )}
       </div>
       {orgId ? <UserGallery userId={orgId} /> : null}
       <Reviews userRecensions={userRecensions} />
